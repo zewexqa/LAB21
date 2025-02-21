a47a4e79042b38e68afab10d5f922f4843f15573
zewexqa <154309211+zewexqa@users.noreply.github.com>
2025-02-21 17:05:03 +0700

    Update average.cpp

--- average.cpp
+++ average.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        cout << "Please input numbers to find average.\n";
+        return 0;
+    }
+    
+    double sum = 0;
+    for (int i = 1; i < argc; i++) {
+        sum += atof(argv[i]);
+    }
+    
+    double avg = sum / (argc - 1);
+    
+    cout << "---------------------------------\n";
+    cout << "Average of " << (argc - 1) << " numbers = " << avg << "\n";
+    cout << "---------------------------------\n";
+    
+    return 0;
+}
 
